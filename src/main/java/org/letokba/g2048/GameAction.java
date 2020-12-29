5415548a9f719455a56014bbb28f2ecf155befc0
Wait <mrfengyong@qq.com>
2020-12-29 11:36:06 +0800

    finish GameCore.java

---
+++ src/main/java/org/letokba/g2048/GameAction.java
@@ -0,0 +1,9 @@
+package org.letokba.g2048;
+
+/**
+ * @author Yong
+ * @date 2020/12/29
+ */
+public class GameAction {
+
+}
--- src/main/java/org/letokba/g2048/GameCore.java
+++ src/main/java/org/letokba/g2048/GameCore.java
@@ -1,10 +1,14 @@
 package org.letokba.g2048;
 
+import java.util.Arrays;
+
 /**
  * @author Wait
  * @date 2020/12/29
  */
 public class GameCore {
+
+
 
     private static class SquareMatrix {
         private static final int SIZE = 4;
@@ -30,26 +34,80 @@ public class GameCore {
             return i * SIZE + j;
         }
 
+        public boolean isNotZero(int i, int j) {
+            return get(i, j) != 0;
+        }
+
+        public int row() {
+            return SIZE;
+        }
+        private int col() {
+            return SIZE;
+        }
+
+        private void replaceOneRow(int row, int[] array) {
+            System.arraycopy(array, 0, this.array, row * SIZE, SIZE);
+        }
+
     }
 
-    private void transpose() {
-
+    private GameCore transpose(SquareMatrix matrix) {
+        for(int i = 0; i < matrix.row(); i++) {
+            for(int j = i + 1; j < matrix.col(); j++) {
+                int k = matrix.get(i, j);
+                matrix.set(j, i, matrix.get(i, j));
+                matrix.set(i, j, k);
+            }
+        }
+        return this;
     }
 
-    private void hFlip() {
-
+    private GameCore hFlip(SquareMatrix matrix) {
+        int col = matrix.col();
+        for(int i = 0; i < matrix.row(); i++) {
+            for(int j = 0; j < matrix.col() / 2; j++) {
+                int k = matrix.get(i, j);
+                matrix.set(i, j, matrix.get(i, col - j));
+                matrix.set(i, col - j, k);
+            }
+        }
+        return this;
     }
 
-    private void vFlip() {
-
+    private GameCore vFlip(SquareMatrix matrix) {
+        return this.transpose(matrix).hFlip(matrix);
     }
 
-    private void clearZeros() {
 
+
+    private GameCore clearZeros(SquareMatrix matrix) {
+        int[] cap = new int[matrix.col()];
+        int k = 0;
+        for(int i = 0; i < matrix.row(); i++) {
+            for(int j = 0; j < matrix.col(); j++) {
+                if(matrix.isNotZero(i, j)){
+                    cap[k++] = matrix.get(i, j);
+                }
+            }
+            matrix.replaceOneRow(i, cap);
+            Arrays.fill(cap, 0);
+        }
+        return this;
     }
 
-    private void mergeSame() {
-
+    private GameCore mergeSame(SquareMatrix matrix) {
+        for(int i = 0; i < matrix.row(); i++) {
+            for(int j = 0; j < matrix.col() - 1; j++) {
+                int pre = matrix.get(i, j);
+                int next = matrix.get(i, j + 1);
+                if(pre == next) {
+                    matrix.set(i, j, pre + next);
+                    matrix.set(i, j + 1, 0);
+                    j++;
+                }
+            }
+        }
+        return this;
     }
 
 
